05a804ca17cde5b1024ef85994b57c8d13ec2bbf
karlobusa <karlo.busa@gmail.com>
2022-03-17 19:45:44 +0100

    Added new class

---
+++ ConsoleApp2/kvadriraj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class kvadriraj
+    {
+        void ispis()
+        {
+            Console.WriteLine("Nesto");
+        }
+    }
+}
